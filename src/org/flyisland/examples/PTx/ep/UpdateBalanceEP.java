4baebcd7841660688a5c7ac97a1e157a48291b48
flyisland <flyisland@gmail.com>
2013-04-30 00:16:05 +0800

    new EP UpdateBalanceEP

---
+++ src/org/flyisland/examples/PTx/ep/UpdateBalanceEP.java
@@ -0,0 +1,21 @@
+package org.flyisland.examples.PTx.ep;
+
+import java.io.Serializable;
+
+import com.tangosol.util.InvocableMap.Entry;
+import com.tangosol.util.processor.AbstractProcessor;
+
+public class UpdateBalanceEP extends AbstractProcessor implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4231456472129888799L;
+
+	@Override
+	public Object process(Entry entry) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
