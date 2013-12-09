0ee906389687d3fc51e46c4aa7f8f7e88bb23af4
flyisland <flyisland@gmail.com>
2013-12-09 10:50:12 +0800

    add sleeping function into NullEP

--- src/org/flyisland/examples/PTx/ep/NullEP.java
+++ src/org/flyisland/examples/PTx/ep/NullEP.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.tangosol.io.pof.annotation.Portable;
+import com.tangosol.io.pof.annotation.PortableProperty;
 import com.tangosol.util.InvocableMap.Entry;
 import com.tangosol.util.processor.AbstractProcessor;
 
@@ -17,8 +18,15 @@ public class NullEP extends AbstractProcessor {
 	static Logger logger = LogManager.getLogger(NullEP.class.getName());
 	private static final long serialVersionUID = 6698280756087317488L;
 
+	@PortableProperty(0)	private String	ops;
+	
 	public NullEP() {
 		super();
+	}
+
+	public NullEP(String ops) {
+		super();
+		this.ops = ops;
 	}
 
 	@Override
@@ -31,10 +39,19 @@ public class NullEP extends AbstractProcessor {
 	@Override
 	public Map processAll(Set setEntries) {
 		logger.trace("===> Entered "+this.toString());
+		int	i = 0;
 		for (Iterator iter = setEntries.iterator(); iter.hasNext(); )
 		{
+			i++;
 			Entry entry = (Entry) iter.next();
-			logger.trace(entry.getKey()+"="+entry.getValue());		
+			logger.trace(i+": "+entry.getKey()+"="+entry.getValue());		
+			if (ops.equalsIgnoreCase("sleep")){
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+			}
 		}
 		logger.trace("===> Exited: "+this.toString());
 		return null;
